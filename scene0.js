42e5674272e9f076cd15118d6195bcc27d1137ae
david2006-arch <davi.d2006@aluno.ifsc.edu.br>
2026-03-25 10:08:04 -0300

    Update scene0.js

--- scene0.js
+++ scene0.js
@@ -10,26 +10,54 @@ class scene0 extends Phaser.Scene {
   preload() {
     this.load.setPath("assets/");
 
-    this.load.tilemapTiledJSON("map", "map.json");
+    this.load.tilemapTiledJSON("MapaFase1", "MapaFase1.JSON");
 
-    this.load.spritesheet("android", "SpaceStation_Android_Sheet.png", {
-      frameWidth: 32,
-      frameHeight: 32,
-    });
+    this.load.image("2", "2.png");
+
+    this.load.image("3 (1)", "3.png");
+
+    this.load.image("4", "4.png");
+
+    this.load.image("6", "6.png");
+
+    this.load.image("Arc", "Arc.png");
+
+    this.load.image("houded2", "houded2.png");
+
+    this.load.image("House Inside", "House_Inside.png");
+
+    this.load.image("House Outside", "House_Outside.png");
+
+    this.load.image("houses", "houses.png");
+
+    this.load.image("houses1", "houses1.png");
+
+    this.load.image("houses2", "houses2.png");
+
+    this.load.image("road&lamps", "road&lamps.png");
+
+    this.load.image("Sakura Tree", "Sakura_Tree.gif");
+
+    this.load.image("SCS_Background_Sunset_01", "SCS_Background_Sunset_01.png");
+
+    //this.load.spritesheet("android", "SpaceStation_Android_Sheet.png", {
+    //frameWidth: 32,
+    //frameHeight: 32,
+    //});
     this.load.spritesheet("character", "SpaceStation_Character_Sheet.png", {
       frameWidth: 32,
       frameHeight: 32,
     });
-    this.load.image("objects", "SpaceStation_Objects.png");
-    this.load.spritesheet("projectiles", "SpaceStation_Projectiles_Sheet.png", {
-      frameWidth: 32,
-      frameHeight: 32,
-    });
-    this.load.image("tileset", "SpaceStation_Tileset.png");
-    this.load.spritesheet("turret", "SpaceStation_Turret_Sheet.png", {
-      frameWidth: 32,
-      frameHeight: 32,
-    });
+    //this.load.image("objects", "SpaceStation_Objects.png");
+    //this.load.spritesheet("projectiles", "SpaceStation_Projectiles_Sheet.png", {
+    //frameWidth: 32,
+    //frameHeight: 32,
+    //});
+    //this.load.image("tileset", "SpaceStation_Tileset.png");
+    //this.load.spritesheet("turret", "SpaceStation_Turret_Sheet.png", {
+    //frameWidth: 32,
+    //frameHeight: 32,
+    //});
     this.load.spritesheet("buttons", "buttons.png", {
       frameWidth: 32,
       frameHeight: 32,
@@ -46,69 +74,89 @@ class scene0 extends Phaser.Scene {
   }
 
   create() {
-    this.tilemap = this.make.tilemap({ key: "map" });
+    this.tilemap = this.make.tilemap({ key: "MapaFase1" });
 
-    this.tilesetTileset = this.tilemap.addTilesetImage("tileset");
-    this.tilesetObjects = this.tilemap.addTilesetImage("objects");
+    this.tileset2 = this.tilemap.addTilesetImage("2");
+    this.tileset3 = this.tilemap.addTilesetImage("3 (1)");
+    this.tileset4 = this.tilemap.addTilesetImage("4");
+
+    this.tileset6 = this.tilemap.addTilesetImage("6");
+    this.tilesetArc = this.tilemap.addTilesetImage("Arc");
+
+    this.tilesetHouded2 = this.tilemap.addTilesetImage("houded2");
+    this.tilesetHouseInside = this.tilemap.addTilesetImage("House Inside");
+    this.tilesetHouseOutside = this.tilemap.addTilesetImage("House Outside");
+    this.tilesetHouses = this.tilemap.addTilesetImage("houses");
+    this.tilesetHouses1 = this.tilemap.addTilesetImage("houses1");
+    this.tilesetHouses2 = this.tilemap.addTilesetImage("houses2");
+    this.tilesetRoadLamps = this.tilemap.addTilesetImage("road&lamps");
+    this.tilesetSakuraTree = this.tilemap.addTilesetImage("Sakura Tree");
+    this.tilesetSunset = this.tilemap.addTilesetImage(
+      "SCS_Background_Sunset_01",
+    );
     /*
     this.tilesetTurret = this.tilemap.addTilesetImage("turret");
     this.tilesetCharacter = this.tilemap.addTilesetImage("character");
     this.tilesetAndroid = this.tilemap.addTilesetImage("android");
     */
 
-    this.layerBackground = this.tilemap.createLayer("background", [
-      this.tilesetTileset,
+    this.layerbackground0 = this.tilemap.createLayer("background 0", [
+      this.tilesetSunset,
     ]);
-    this.layerGround = this.tilemap.createLayer("ground", [
-      this.tilesetTileset,
-      this.tilesetObjects,
+    this.layerbackground1 = this.tilemap.createLayer("background 1", [
+      this.tileset4,
     ]);
-    this.layerRoof = this.tilemap.createLayer("roof", [
-      this.tilesetTileset,
-      this.tilesetObjects,
+    this.layerbackground2 = this.tilemap.createLayer("background 2", [
+      this.tileset2,
+      this.tileset4,
     ]);
-    this.layerWalls = this.tilemap.createLayer("walls", [
-      this.tilesetTileset,
-      this.tilesetObjects,
+    this.layerbackground3 = this.tilemap.createLayer("background 3", [
+      this.tileset3,
     ]);
-    this.layerWallsUnder = this.tilemap.createLayer("walls_under", [
-      this.tilesetTileset,
-      this.tilesetObjects,
+    this.layerbackground4 = this.tilemap.createLayer("background 4", [
+      this.tileset2,
+      this.tileset4,
     ]);
-    this.layerWallsOver = this.tilemap.createLayer("walls_over", [
-      this.tilesetTileset,
-      this.tilesetObjects,
+    this.layerbackground5 = this.tilemap.createLayer("background 5", [
+      this.tileset2,
+      this.tileset4,
+      this.tileset6,
+      this.tilesetSakuraTree,
+      this.tilesetHouses1,
+      this.tilesetHouseOutside,
+      this.tilesetHouseInside,
     ]);
-    this.layerLamps = this.tilemap.createLayer("lamps", [
-      this.tilesetTileset,
-      this.tilesetObjects,
+    this.layercasas = this.tilemap.createLayer("casas", [
+      this.tilesetHouseOutside,
+      this.tilesetHouseInside,
+      this.tilesetHouded2,
+      this.tilesetHouses1,
+      this.tilesetHouses,
+      this.tilesetHouses2,
     ]);
-    this.layerWindows = this.tilemap.createLayer("windows", [
-      this.tilesetTileset,
-      this.tilesetObjects,
+    this.layerMarquinhosDojo = this.tilemap.createLayer("marquinhosa dojo", [
+      this.tilesetArc,
     ]);
-    this.layerObjects = this.tilemap.createLayer("objects", [
-      this.tilesetTileset,
-      this.tilesetObjects,
-    ]);
-    this.layerTeletransport = this.tilemap.createLayer("teletransport", [
-      this.tilesetTileset,
-      this.tilesetObjects,
-    ]);
+    this.layerrua = this.tilemap.createLayer("rua", [this.tilesetRoadLamps]);
+    this.layerobjetos = this.tilemap.createLayer("objetos", [this.tilesetArc]);
+    //this.layerTeletransport = this.tilemap.createLayer("teletransport", [
+    //this.tilesetTileset,
+    //this.tilesetObjects,
+    //]);
     /*
     this.layerCharacter = this.tilemap.createLayer("character", [
       this.tilesetCharacter,
     ]);
     this.layerEnemy = this.tilemap.createLayer("enemy", [this.tilesetAndroid]);
     */
-    this.layerPlatform = this.tilemap.createLayer("platform", [
-      this.tilesetTileset,
-      this.tilesetObjects,
-    ]);
-    this.layerShelf = this.tilemap.createLayer("shelf", [
-      this.tilesetTileset,
-      this.tilesetObjects,
-    ]);
+    //this.layerPlatform = this.tilemap.createLayer("platform", [
+    //this.tilesetTileset,
+    //this.tilesetObjects,
+    //]);
+    //this.layerShelf = this.tilemap.createLayer("shelf", [
+    //this.tilesetTileset,
+    //this.tilesetObjects,
+    //]);
 
     this.player = this.physics.add.sprite(150, 656, "character", 0);
 
@@ -158,32 +206,32 @@ class scene0 extends Phaser.Scene {
 
     this.player.setCollideWorldBounds(true);
 
-    this.layerGround.setCollisionByProperty({ collides: true });
-    this.physics.add.collider(this.player, this.layerGround);
+    this.layercasas.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.player, this.layercasas);
 
-    this.layerRoof.setCollisionByProperty({ collides: true });
-    this.physics.add.collider(this.player, this.layerRoof);
+    this.layerMarquinhosDojo.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.player, this.layerMarquinhosDojo);
 
-    this.layerWalls.setCollisionByProperty({ collides: true });
-    this.physics.add.collider(this.player, this.layerWalls);
+    this.layerrua.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.player, this.layerrua);
 
-    this.layerWallsUnder.setCollisionByProperty({ collides: true });
-    this.physics.add.collider(this.player, this.layerWallsUnder);
+    // this.layerWallsUnder.setCollisionByProperty({ collides: true });
+    // this.physics.add.collider(this.player, this.layerWallsUnder);
 
-    this.layerWallsOver.setCollisionByProperty({ collides: true });
-    this.physics.add.collider(this.player, this.layerWallsOver);
+    // this.layerWallsOver.setCollisionByProperty({ collides: true });
+    // this.physics.add.collider(this.player, this.layerWallsOver);
 
-    this.layerLamps.setCollisionByProperty({ collides: true });
-    this.physics.add.collider(this.player, this.layerLamps);
+    // this.layerLamps.setCollisionByProperty({ collides: true });
+    // this.physics.add.collider(this.player, this.layerLamps);
 
-    this.layerPlatform.setCollisionByProperty({ collides: true });
-    this.physics.add.collider(this.player, this.layerPlatform);
-    this.layerPlatform.forEachTile((tile) => {
-      if (tile.properties.collides) {
-        // left, right, up, down
-        tile.setCollision(false, false, true, false);
-      }
-    });
+    // this.layerPlatform.setCollisionByProperty({ collides: true });
+    // this.physics.add.collider(this.player, this.layerPlatform);
+    // this.layerPlatform.forEachTile((tile) => {
+    //   if (tile.properties.collides) {
+    //     // left, right, up, down
+    //     tile.setCollision(false, false, true, false);
+    //   }
+    // });
 
     // this.music = this.sound.add("music", { loop: true }).play();
     this.laser = this.sound.add("laser");
